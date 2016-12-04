295503d19e9c571f8272d8a56aecd208a86346db
Paul Vaughan <paulieboo@gmail.com>
2016-12-04 09:42:00 +0000

    AoC 2015, Day Four.

---
+++ 2015/day4.php
@@ -0,0 +1,26 @@
+<?php
+
+// Advent of Code 2015. Day Four.
+
+$instructions = 'yzbqklnj';
+
+for ($j = 1; $j <= 10000000; $j++) {
+    $plaintext = $instructions . $j;
+    $hash = md5($plaintext);
+    if (preg_match('/^00000/', $hash)) {
+        echo 'First hash with "00000" at the start: ' . $hash . ' from input: "' . $plaintext . '".' . PHP_EOL;
+        break;
+    }
+}
+
+for ($j = 1; $j <= 10000000; $j++) {
+    $plaintext = $instructions . $j;
+    $hash = md5($plaintext);
+    if (preg_match('/^000000/', $hash)) {
+        echo 'First hash with "000000" at the start: ' . $hash . ' from input: "' . $plaintext . '".' . PHP_EOL;
+        break;
+    }
+}
+
+// First hash with "00000" at the start: 000002c655df7738246e88f6c1c43eb7 from input: "yzbqklnj282749".
+// First hash with "000000" at the start: 0000004b347bf4b398b3f62ace7cd301 from input: "yzbqklnj9962624".
