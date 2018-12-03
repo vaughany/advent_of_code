5424cccbfbd4b646aa2233864ebb892a8207eabd
Paul Vaughan <paulieboo@gmail.com>
2018-12-03 17:15:24 +0000

    AoC 2018, Day Two.

---
+++ 2018/day2.php
@@ -0,0 +1,313 @@
+#!/usr/bin/php
+<?php
+
+// Advent of Code 2018. Day Two. http://adventofcode.com/2018/day/2
+
+$out1 = $two = $three = 0;
+$out2 = '';
+
+$instructions = 'mgdsdwqulhrinkatfjoyzxcbvq
+megsdwpulhrinkasfjdyzxcbvk
+megsdwpulhmibkatfjoyzccbvq
+megsdwtuohrinkatfjoyzecbvq
+megsdwpulhdinkatfyoyzxcbvb
+megszwpulhrirkatfjoyexcbvq
+eegsdwpulhzinkatfjoyzxdbvq
+megsdwpulhginkatfjoyqucbvq
+leqsdwpulhrinkatfeoyzxcbvq
+megsdwpblhrandatfjoyzxcbvq
+megrdwpudhrinvatfjoyzxcbvq
+mlgsdwpulhrinkatfuoyzxcbyq
+mensdwpunhrinsatfjoyzxcbvq
+megsdwpnlhrinqatfjoyzxcbuq
+megsdwpnlhrinkatfqoysxcbvq
+megsdwpulhrinkjtfjoywxobvq
+megsdwpclhbinkaffjoyzxcbvq
+mersdwpulhrinkattjoyzxcbdq
+megsddpwohrinkatfjoyzxcbvq
+mejsdwpulhrinratfjoyzxcbvl
+megsdwpujhrinkatfjkbzxcbvq
+megsdwpujhrinkatfjoyircbvq
+megsdwgulhrinkatojoyzxcbvm
+qegsdwpulhrinkatfjoyzxggvq
+megsdwpflhrinkatfjsazxcbvq
+megsdwpulhridkagfjoyzzcbvq
+mejsdwpulhriykatfjoyyxcbvq
+measdwpulhriakatfjoyzacbvq
+mebsdwpulhrinkatfjoyzxmyvq
+megsdwpflnrinkatfjoyzxcbxq
+megsdwpulhxinuatfjoytxcbvq
+megsdwsflhrinkatfjoyzxcmvq
+megsdwpuuhritkarfjoyzxcbvq
+megsdwpulhrinkazfjoyexcjvq
+megsdwpulhrifkatfjokzxcbvc
+megsdwmulvrinkatfjoyzxcblq
+meggdwpulhrfnkatfjoybxcbvq
+megsdwptlhrindatyjoyzxcbvq
+megsdwpulhrinkauqjoyzxibvq
+megsdwddlhrifkatfjoyzxcbvq
+megddwpulhrinkagfjoyzxcbva
+messdupulhrinkatfjoyzxqbvq
+megsdwpuahrinkatfjotzxchvq
+megsewpulhrankathjoyzxcbvq
+megsdwpulhrinkatfjoynqcbvv
+megsdwpulhrifkatfjotzxcbtq
+megsdjpulcrinkitfjoyzxcbvq
+fefsowpulhrinkatfjoyzxcbvq
+megsdwpulapinkatfjoyzvcbvq
+mejsdwpularinkatfjonzxcbvq
+megsxwpllrrinkatfjoyzxcbvq
+megsdlpulxvinkatfjoyzxcbvq
+megsdwpulhriekztfjoyzxxbvq
+megspwpulhrinuatfjoyzxcgvq
+megqdwpulhrinkatfjoyzujbvq
+megsdtpuehrinkatfjoyzxtbvq
+megsdwjulhrmnmatfjoyzxcbvq
+megjdwpulhrinkztfjoyzpcbvq
+megsdwpuljrinkawfjlyzxcbvq
+mewsdwpulhtinkatfjodzxcbvq
+megsdwpulhrinkatfcoizxcmvq
+meisdwpulhrinkatawoyzxcbvq
+megshwpulhrinkatfjoyzxcbtz
+megsdwpulhfinkatfjowzxcbvj
+megodwpulhrcskatfjoyzxcbvq
+megydxpulhrinkatfjofzxcbvq
+megsdwpulhrinkatljoytxdbvq
+mwgsdwpulyrinkatfjoyzxvbvq
+meesdwpulhrinkatfjcwzxcbvq
+cegsdwpwlhrinkatfioyzxcbvq
+megsdwpulhrinkatfqhyxxcbvq
+megsrrpulhrxnkatfjoyzxcbvq
+megsdgpulhrinkatfjoypxcbnq
+xegsdwpulhrinkaxkjoyzxcbvq
+mngsdwpulhrenkatfjofzxcbvq
+mecsdwpulhrinkabfjoozxcbvq
+pegsdkpulhrinkatfuoyzxcbvq
+megpdwpughrinkatfjoyzxzbvq
+mxgrdwpulhrinyatfjoyzxcbvq
+megsdwpulhpigkatfjoyzxcbfq
+mhgsdwpulhrinkagfjoxzxcbvq
+megsdwpblhtinkatfjqyzxcbvq
+megsawvulhrinkatfjoyzxcfvq
+metsuwpulhrinkatfjowzxcbvq
+mexsdwpulhrinkatsjoyzxvbvq
+megtdwpulhrinkatfjoizxcpvq
+megidwpjlhrinkatfdoyzxcbvq
+megsdwpuljrivkatfjoyqxcbvq
+mespdppulhrinkatfjoyzxcbvq
+megsdcpulhminkatftoyzxcbvq
+megsdwpulhriskatfjayzlcbvq
+mejsdwpulhrinkatfvoyzxibvq
+megfdwputhriykatfjoyzxcbvq
+megsdwparhrinkbtfjoyzxcbvq
+mtgsdwpulhmingatfjoyzxcbvq
+megsdwjulhrinkatfjtyzxtbvq
+megsvwpulhrinkatfjoyzecbvf
+megsdwpulprinkatyqoyzxcbvq
+zegsdwpulhwinkatfjoyzxcfvq
+megsdwiulhrinkatfjoyzxcjcq
+aegsdwpulirinkatfjoyvxcbvq
+mepsdwculbrinkatfjoyzxcbvq
+msgsdcpulhrinkctfjoyzxcbvq
+megsdwpulhrinkatfjoozxfevq
+megscwpulhrinkatmjoyzxcbsq
+megsdwpulhrinkltfjoyzxtmvq
+megsddpulhrixkatfjyyzxcbvq
+megsdwpulhrinkctfjoqzhcbvq
+megsbapblhrinkatfjoyzxcbvq
+megsdwpulhrinkatfjoyyxgpvq
+mwpsdwpulhrinkutfjoyzxcbvq
+megsdwpulhrinvnlfjoyzxcbvq
+megsdwsulorinkakfjoyzxcbvq
+mbgsdwpulhrinkktfjoyzxcbvg
+megddwpulhrindatfjoyzxcbvr
+memsdwpulhrinkatqjoyzxcbvo
+megsdwpulbrinkarfjoyzxcbxq
+megsdwpulhrilkatfjoynxcgvq
+mxgsdzpulhrinkatfjoydxcbvq
+megsawpulhrinkotfjoyzacbvq
+measdwpulhrinkctvjoyzxcbvq
+medsdwpulhtinkaqfjoyzxcbvq
+megsdwsulhrknkatfjoylxcbvq
+megadwpulhrinkatfjkyzxcbvh
+mygsdwpulhrinkatfboyzxcavq
+megsdwpulhrinkaufjxyzccbvq
+mtgsdwpulhrunkatfkoyzxcbvq
+megsdwpulhrinkamfwryzxcbvq
+meesdwpulhrgnkatnjoyzxcbvq
+megddwpolhrinkatfjoyixcbvq
+megsdwpulhrinsatfjoyzicwvq
+mmgsdwpulhrinkltfjoyzxcgvq
+meisdwpulhhinkatfjoyzxubvq
+megpdwpulhrinkatfroyzxcbkq
+megszwpuhhrinkatfjoyzxcbzq
+mersdwpulhrikcatfjoyzxcbvq
+mjgsdwpulhrinratfjoyzxcbvl
+megsdwnulkxinkatfjoyzxcbvq
+megsdwpulhrinkatlaoyzxpbvq
+megsdwpulhribkitfjjyzxcbvq
+megsdwpzlhrinkalfjoyzkcbvq
+megsdwpulhrinkutfjoyzxcdnq
+mfgpdwputhrinkatfjoyzxcbvq
+oegshwpclhrinkatfjoyzxcbvq
+megvdwxulhrinkatfjowzxcbvq
+mrgydwpulhrinkatfjoyzxcbvl
+megsdwpuqhrinkaxfjoyzxcbfq
+megsdwpughrinixtfjoyzxcbvq
+megsdwpslxrinkatfjoyzxcbvl
+megsdwpukhrinkatfjovzxnbvq
+megsdwpulnrinkrtojoyzxcbvq
+megsdwpuvhrinkatfjjyzxclvq
+vegrdwpulhrinzatfjoyzxcbvq
+melsdwwulvrinkatfjoyzxcbvq
+megsdwpulhrizkbtfjoyzjcbvq
+megsdwpulhrinlatfjoyzkcbvr
+megsdweulhrinkaofqoyzxcbvq
+megsdcpulhrifkatfdoyzxcbvq
+negsdwpulhainkztfjoyzxcbvq
+megsdwpklhrinkttfjoyzxvbvq
+megsdwpothrdnkatfjoyzxcbvq
+megsdwpulvrinkatfaoyzxcbvr
+mewsdwpulhrinkatfooyzxtbvq
+megtdwpulhrinkatfxoyzxcbvy
+zegsdwpulhrinkamljoyzxcbvq
+mkgodwpulhrcnkatfjoyzxcbvq
+megsdepularinkatfjoyzxybvq
+segsdwpulhrinkatfmoyzycbvq
+mbgsdwpulhrsnkztfjoyzxcbvq
+megsdwpmlhrinkatfjxyzhcbvq
+megsdwpulhrinkatfboyqxmbvq
+megsdwpulhrinkadtpoyzxcbvq
+megsdmpulhrjnkatfjoyzxwbvq
+megsdwpulhrivkatfroywxcbvq
+megsdwdulhrinkatpjwyzxcbvq
+megsdwpylhrdrkatfjoyzxcbvq
+megsdwpulhrvnkptfjoyzlcbvq
+megsdwpuihridkatfjuyzxcbvq
+megsdwpulhbiikatfjxyzxcbvq
+meggdwpulurinkatfjoyaxcbvq
+mehsnwpulcrinkatfjoyzxcbvq
+megsdwpulhrinkatfjoczdclvq
+megsdwjulhrinkatfhkyzxcbvq
+megsdwpulhrinoatfjoyzxcwvy
+megpdwpelhrfnkatfjoyzxcbvq
+megsjwpulhrwnkatfjdyzxcbvq
+megsdwpulhrbniatfjoyzxcbpq
+megsdwpulhrwnkatfjoydxcpvq
+megsdwrulhrinkatfjvyixcbvq
+yegvdwpulhtinkatfjoyzxcbvq
+meosdwpulhrinkatfjhyzxsbvq
+mjgsdwpulhrinkatfjoyyxcjvq
+megsdwpulhrinkatfnoyzlmbvq
+mehsdfkulhrinkatfjoyzxcbvq
+megsdwpuahrinkatfyoyzxcwvq
+megsdwpulhyinkatfjobzkcbvq
+megsdwpjlhrinkatfnoyzscbvq
+megsdwiulhrinqatfjoyzxcavq
+megsdwpulurinkatfjoykxcbmq
+megsdwpmlhvinkatfjotzxcbvq
+megswwpulhrinfatfjoytxcbvq
+mxgsdwpulhrrnkatfjoyhxcbvq
+megubwpulhrinkaafjoyzxcbvq
+megsdwpulhrdnsztfjoyzxcbvq
+megsdwpulhronkgtfjhyzxcbvq
+megsdwzulhfinratfjoyzxcbvq
+megsdwpulhrinkrtfjqyixcbvq
+megspwpulhrinkatfjoywxcavq
+megmfjpulhrinkatfjoyzxcbvq
+lelsdwpulhrinsatfjoyzxcbvq
+megsdwnultrinkatfjoyzxybvq
+megsdwpulhdnnkatfjpyzxcbvq
+megsdwpulhzinkatfjoyzhcpvq
+megoddpulhrilkatfjoyzxcbvq
+megsdvpulhrwnkatfjoyzxcbdq
+megsdwpulhmikkatfjoyzxcbvr
+meesdwpulhrinkaofjoazxcbvq
+megndwhulhrinfatfjoyzxcbvq
+megsdwpulhjihkatfjoyzxcbvb
+jegsdwpulhrinkattjoszxcbvq
+megsdwpulhrenkatfjodzxcbvp
+mefsdwpulhrinkatfjoyzxcczq
+megsdwpulhrknkatfjovzxcbvb
+megytwpulhrinkatfjoyzxcbvo
+csgsdwpulhrinkatdjoyzxcbvq
+megsbwpulhrinkazfjtyzxcbvq
+mrgsdwpulhjinkatfjoyzxcbvm
+megsywzulhrinkatfjofzxcbvq
+megxdwpulhrndkatfjoyzxcbvq
+mcgsdwpuphrixkatfjoyzxcbvq
+megsdlpulxvinkatfvoyzxcbvq
+megsdwpulhrinfatfjoyzicbvo
+megwgwpulhrinpatfjoyzxcbvq
+mggsdwpulhrinkatfjtrzxcbvq
+msgsdwpulirinkatfjoyzxcbaq
+megsdwpulhrineatfeoyzxcbvl
+megsdnpulhrinkatojoyzxcbvb
+mfgsdwpulhdinkatmjoyzxcbvq
+megsdwpulhrinkawfjuyzxcbvg
+megsdwpulhrincatfjoyzxcxvu
+mevsdwzplhrinkatfjoyzxcbvq
+megskweulhfinkatfjoyzxcbvq
+mebsdvpulhrinkatfjoyzxcbkq
+megddwiulhrinkatfioyzxcbvq
+mygsdwpulhqinkatfooyzxcbvq
+megsdweulhrfnkatfjoybxcbvq
+mrgsdhpulmrinkatfjoyzxcbvq
+megsdwkukhrinkatftoyzxcbvq
+megsqwpulhrinfatfjoyzxebvq
+megsdwpulhriskanfjoyzxctvq';
+
+$len = 26;
+
+// Part One.
+$split_lineend = explode(PHP_EOL, $instructions);
+foreach ($split_lineend as $line) {
+    $letters = [];
+    for ($j=0; $j <= ($len - 1); $j++) {
+        $l = substr($line, $j, 1);
+        if (isset($letters[$l])) {
+            $letters[$l]++;
+        } else {
+            $letters[$l] = 1;
+        }
+    }
+    $twodone = $threedone = false;
+    foreach ($letters as $key => $value) {
+        if ($value == 2 && $twodone == false) {
+            $two++;
+            $twodone = true;
+        } else if ($value == 3 && $threedone == false) {
+            $three++;
+            $threedone = true;
+        } else {
+            unset ($letters[$key]);
+        }
+    }
+    $out1 = $two * $three;
+}
+
+// Part Two.
+$split_lineend = explode(PHP_EOL, $instructions);
+foreach ($split_lineend as $line) {
+    for ($j = 0, $count = count($split_lineend) - 1; $j <= $count; $j++) {
+        $diffs = 0;
+        $out2 = '';
+        for ($k = 0; $k <= $len - 1; $k++ ) {
+            if (substr($line, $k, 1) !== substr($split_lineend[$j], $k, 1)) {
+                $diffs++;
+            } else {
+                $out2 .= substr($line, $k, 1);
+            }
+        }
+        if ($diffs == 1) {
+            break 2;
+        }
+    }
+}
+
+// Part One: 5456
+// Part Two: megsdlpulxvinkatfoyzxcbvq
+echo 'Advent of Code 2018, Day Two.' . PHP_EOL;
+echo 'Part One: ' . $out1 . PHP_EOL;
+echo 'Part Two: ' . $out2 . PHP_EOL;
+echo 'Memory usage: ' . number_format(memory_get_peak_usage() / 1024) . 'Kb' . PHP_EOL;
