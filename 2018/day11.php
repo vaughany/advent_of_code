3564285bf7e47222224d366948963fbc6d1b3ddd
Paul <paulieboo@gmail.com>
2019-09-06 16:16:05 +0100

    Came back to this in September and just want to not lose everything...

---
+++ 2018/day11.php
@@ -0,0 +1,115 @@
+#!/usr/bin/php
+<?php
+
+// Advent of Code 2018. Day Eleven. http://adventofcode.com/2018/day/11
+
+$timestart = microtime(true);
+
+echo 'Advent of Code 2018, Day Eleven.' . PHP_EOL;
+
+$out1 = $out2 = $largest = $largestx = $largesty = 0;
+$grid = [];
+
+$instructions = 7400;
+
+function draw3x3(int $inx, int $iny) : string {
+    global $grid;
+    $out = PHP_EOL;
+    for ($y = $iny; $y < $iny + 3; $y++ ) {
+        for ($x = $inx; $x < $inx + 3; $x++ ) {
+            if (isset($grid[$x][$y])) {
+                // $out .= $grid[$x][$y]['int'] . "\t";
+                $out .= $grid[$x][$y]['pwr'] . "\t";
+            } else {
+                throw new Exception("Grid element doesn't exist");
+            }
+        }
+        $out .= PHP_EOL;
+    }
+    return $out . PHP_EOL;
+}
+
+function sum_xys(int $inx, int $iny, int $s) : int {
+    global $grid;
+    $out = 0;
+    for ($y = $iny; $y < $iny + $s; $y++) {
+        for ($x = $inx; $x < $inx + $s; $x++) {
+            if (isset($grid[$x][$y][$s])) {
+                $out += $grid[$x][$y][$s]['pwr'];
+            } else {
+                throw new Exception("Grid element doesn't exist");
+            }
+        }
+    }
+    return $out;
+}
+
+function get_hundred(int $in) : int {
+    $in = (string) $in;
+    if (strlen($in) < 3) {
+        return 0;
+    }
+    return (int) substr($in, -3, 1);
+}
+
+$s = 3;
+for ($y = 1; $y <= 300; $y++ ) {
+    for ($x = 1; $x <= 300; $x++ ) {
+        $grid[$x][$y][$s]['rid'] = $x + 10;
+        $grid[$x][$y][$s]['pwr'] = ($grid[$x][$y][$s]['rid'] * $y) + $instructions;
+        $grid[$x][$y][$s]['pwr'] = $grid[$x][$y][$s]['pwr'] * $grid[$x][$y][$s]['rid'];
+        $grid[$x][$y][$s]['pwr'] = get_hundred($grid[$x][$y][$s]['pwr']) - 5;
+    }
+}
+
+// Part One.
+for ($y = 1; $y <= 298; $y++ ) {
+    for ($x = 1; $x <= 298; $x++ ) {
+        $grid[$x][$y][$s]['totalpwr'] = sum_xys($x, $y, $s);
+        if ($grid[$x][$y][$s]['totalpwr'] > $largest) {
+            $largest = $grid[$x][$y][$s]['totalpwr'];
+            $largestx = $x;
+            $largesty = $y;
+        }
+    }
+}
+$out1 = "{$largestx},{$largesty}";
+
+// Part One: "34,72"
+$timepartone = microtime(true);
+echo "Part One:\t" . $out1 . "\t(time taken:\t" . number_format($timepartone - $timestart, 6) . 's)' . PHP_EOL;
+
+// Part Two.
+for ($s = 1; $s <= 300; $s++) {
+    $grid = [];
+    echo $s . '.';
+    for ($y = 1; $y <= 300; $y++ ) {
+        for ($x = 1; $x <= 300; $x++ ) {
+            $grid[$x][$y][$s]['rid'] = $x + 10;
+            $grid[$x][$y][$s]['pwr'] = ($grid[$x][$y][$s]['rid'] * $y) + $instructions;
+            $grid[$x][$y][$s]['pwr'] = $grid[$x][$y][$s]['pwr'] * $grid[$x][$y][$s]['rid'];
+            $grid[$x][$y][$s]['pwr'] = get_hundred($grid[$x][$y][$s]['pwr']) - 5;
+        }
+    }
+    for ($y = 1; $y <= 300 - ($s - 1); $y++ ) {
+        for ($x = 1; $x <= 300 - ($s - 1); $x++ ) {
+            $grid[$x][$y][$s]['totalpwr'] = sum_xys($x, $y, $s);
+            if ($grid[$x][$y][$s]['totalpwr'] > $largest) {
+                $largest = $grid[$x][$y][$s]['totalpwr'];
+                $largestx = $x;
+                $largesty = $y;
+                $largests = $s;
+
+                echo PHP_EOL . "{$largestx},{$largesty},{$largests}" . PHP_EOL;
+            }
+        }
+    }
+}
+$out2 = "{$largestx},{$largesty},{$largests}";
+
+$timeend = microtime(true);
+
+// Part Two: "233,187,13"
+echo "Part Two:\t" . $out2 . "\t(time taken:\t" . number_format($timeend - $timepartone, 6) . 's)' . PHP_EOL;
+echo "Memory usage:\t" . number_format(memory_get_peak_usage() / 1024) . 'Kb' . PHP_EOL;
+echo "Time Taken:\t" . number_format($timeend - $timestart, 6) . 's' . PHP_EOL;
---
+++ 2018/day12.php
@@ -0,0 +1,118 @@
+#!/usr/bin/php
+<?php
+
+// Advent of Code 2018. Day Twelve. http://adventofcode.com/2018/day/12
+
+$timestart = microtime(true);
+
+echo 'Advent of Code 2018, Day Twelve.' . PHP_EOL;
+
+$out1 = $out2 = 0;
+
+$pots = '#.#.#...#..##..###.##.#...#.##.#....#..#.#....##.#.##...###.#...#######.....##.###.####.#....#.#..##';
+$instructions = '#...# => #
+....# => .
+##..# => #
+.#.## => #
+##.## => .
+###.# => #
+..... => .
+...#. => .
+.#.#. => #
+#.##. => #
+..#.# => #
+.#... => #
+#.#.. => .
+##.#. => .
+.##.. => #
+#..#. => .
+.###. => .
+..#.. => .
+#.### => .
+..##. => .
+.#..# => #
+.##.# => .
+.#### => .
+...## => #
+#.#.# => #
+..### => .
+#..## => .
+####. => #
+##### => .
+###.. => #
+##... => #
+#.... => .';
+
+
+// $pots = '#..#.#..##......###...###';
+// $instructions = '...## => #
+// ..#.. => #
+// .#... => #
+// .#.#. => #
+// .#.## => #
+// .##.. => #
+// .#### => #
+// #.#.# => #
+// #.### => #
+// ##.#. => #
+// ##.## => #
+// ###.. => #
+// ###.# => #
+// ####. => #';
+//$instructions = '.#..# => #';
+
+$pots = ".....{$pots}.....";
+
+// Part One.
+$ins = [];
+$split_lineend = explode(PHP_EOL, $instructions);
+foreach ($split_lineend as $key => $line) {
+    $tmp = explode(' ', $line);
+    $ins[$key]['note'] = $tmp[0];
+    $ins[$key]['action'] = ($tmp[2] == '#') ? 'add' : 'remove';
+}
+// var_dump($ins);
+
+echo $pots . PHP_EOL;
+for ($k = 1; $k <= 20; $k++) {
+    for ($j = 0; $j <= strlen($pots); $j++) {
+        $slice = substr($pots, $j, 5);
+        if (strlen($slice) != 5) {
+            continue;
+        }
+        foreach ($ins as $i) {
+            // echo 'Checking ' . $i['note'] . ' against ' . $slice . ' : ';
+            if ($i['note'] == $slice) {
+                // echo 'match! ';
+                // change it
+                if ($i['action'] == 'add') {
+                    // echo 'addition' . PHP_EOL;
+                    $pots[$j+2] = '#';
+                } else {
+                    // echo 'subtraction' . PHP_EOL;
+                    $pots[$j+2] = '.';
+                }
+            } else {
+                // echo 'no match' . PHP_EOL;
+            }
+        }
+        // echo PHP_EOL;
+    }
+    echo $pots . PHP_EOL;
+}
+
+$out1 = 0;
+
+// Part One: "34,72"
+$timepartone = microtime(true);
+echo "Part One:\t" . $out1 . "\t(time taken:\t" . number_format($timepartone - $timestart, 6) . 's)' . PHP_EOL;
+
+// Part Two.
+$out2 = 0;
+
+$timeend = microtime(true);
+
+// Part Two: "233,187,13"
+echo "Part Two:\t" . $out2 . "\t(time taken:\t" . number_format($timeend - $timepartone, 6) . 's)' . PHP_EOL;
+echo "Memory usage:\t" . number_format(memory_get_peak_usage() / 1024) . 'Kb' . PHP_EOL;
+echo "Time Taken:\t" . number_format($timeend - $timestart, 6) . 's' . PHP_EOL;
